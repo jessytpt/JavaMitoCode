6edb712fac129f92da611529272a56323a3f1522
Jessenia Toapanta <jessytpt.dev@gmail.com>
2022-03-24 17:11:40 -0500

    add line

--- src/main/java/com/example/service/PersonaServiceImp.java
+++ src/main/java/com/example/service/PersonaServiceImp.java
@@ -1,6 +1,7 @@
 package com.example.service;
 
 import com.example.repository.IPersonaRepo;
+import com.example.repository.PersonaRepoImp;
 
 public class PersonaServiceImp implements IPersonaService{
 
@@ -8,6 +9,7 @@ public class PersonaServiceImp implements IPersonaService{
 
     @Override
     public void registrar(String name) {
+        repo  = new PersonaRepoImp();
         repo.registrar(name);
     }
 }
